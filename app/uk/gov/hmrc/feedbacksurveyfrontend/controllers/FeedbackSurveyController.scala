55e1bd9695a5365f33b80868b907a15ca5af63ba
Grant Robson <grant.robson@digital.hmrc.gov.uk>
2018-10-04 10:54:00 +0100

    PODS-1665 Separate control flow into separate methods

--- app/uk/gov/hmrc/feedbacksurveyfrontend/controllers/FeedbackSurveyController.scala
+++ app/uk/gov/hmrc/feedbacksurveyfrontend/controllers/FeedbackSurveyController.scala
@@ -20,13 +20,12 @@ import models.feedbackSurveyModels._
 import play.api.Play
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc._
-import uk.gov.hmrc.feedbacksurveyfrontend.{FrontendAppConfig, LocalTemplateRenderer}
 import uk.gov.hmrc.feedbacksurveyfrontend.services.OriginService
 import uk.gov.hmrc.feedbacksurveyfrontend.views.html
+import uk.gov.hmrc.feedbacksurveyfrontend.{FrontendAppConfig, LocalTemplateRenderer}
 import uk.gov.hmrc.play.binders.Origin
 import uk.gov.hmrc.play.frontend.controller.FrontendController
 import uk.gov.hmrc.renderer.TemplateRenderer
-import utils.FeedbackSurveySessionKeys._
 import utils.LoggingUtils
 
 
@@ -41,74 +40,123 @@ trait FeedbackSurveyController extends FrontendController with LoggingUtils with
 
   def originService: OriginService
 
-  def ableToDo(origin: String) = Action { implicit request =>
-    Ok(html.feedbackSurvey.ableToDo(formMappings.ableToDoForm, origin))
+  private val ableToDoPage = "ableToDoPage"
+  private val usingServicePage = "usingServicePage"
+  private val aboutServicePage = "aboutServicePage"
+  private val recommendServicePage = "recommendServicePage"
+  private val thankyouPage = "thankyouPage"
+
+  private val modePageLoad = "pageLoad"
+  private val modePageContinue = "pageContinue"
+
+  def nextPage(origin: String, page:String): String = {
+    page match {
+      case `ableToDoPage` => usingServicePage
+      case `usingServicePage` => aboutServicePage
+      case `aboutServicePage` => recommendServicePage
+      case `recommendServicePage` => thankyouPage
+      case _ => usingServicePage
+    }
   }
 
-  def ableToDoContinue(origin: String) =  Action (parse.form(formMappings.ableToDoForm)) { implicit request =>
-        val ableToDoWhatNeeded = request.body.ableToDoWhatNeeded
+  private def renderPageLoad(origin: String, pageId: String)(implicit request:Request[_]): Result = {
+    pageId match {
+      case `ableToDoPage` => Ok(html.feedbackSurvey.ableToDo(formMappings.ableToDoForm, origin))
+      case `usingServicePage` => Ok(html.feedbackSurvey.usingService(formMappings.usingServiceForm, origin))
+      case `aboutServicePage` => Ok(html.feedbackSurvey.aboutService(formMappings.aboutServiceForm, origin))
+      case `recommendServicePage` => Ok(html.feedbackSurvey.recommendService(formMappings.recommendServiceForm, origin))
+      case _ => Redirect(routes.FeedbackSurveyController.thankYou(Origin(origin)))
+    }
+  }
+
+  private def redirectToPage(origin: String, pageId: String)(implicit request:Request[_]): Result = {
+    pageId match {
+      case `usingServicePage` => Redirect(routes.FeedbackSurveyController.usingService(origin))
+      case `aboutServicePage` => Redirect(routes.FeedbackSurveyController.aboutService(origin))
+      case `recommendServicePage` => Redirect(routes.FeedbackSurveyController.recommendService(origin))
+      case `thankyouPage` =>
+        originService.customFeedbackUrl(Origin(origin)) match {
+          case Some(x) => Redirect(x)
+          case None => Redirect(routes.FeedbackSurveyController.thankYou(Origin(origin)))
+        }
+      case _ => Ok(html.feedbackSurvey.ableToDo(formMappings.ableToDoForm, origin))
+    }
+  }
+
+  def ableToDo(origin: String): Action[AnyContent] = Action { implicit request => renderPageLoad(origin, ableToDoPage) }
+
+  def ableToDoContinue(origin: String): Action[AbleToDo] = Action(parse.form(formMappings.ableToDoForm)) { implicit request =>
+    val ableToDoWhatNeeded = request.body.ableToDoWhatNeeded
     audit("feedback-survey", Map("origin" -> origin,
       "ableToDoWhatNeeded" -> ableToDoWhatNeeded.getOrElse("")), eventTypeSuccess)
-    Redirect(routes.FeedbackSurveyController.usingService(origin))
+    redirectToPage(origin, nextPage(origin, ableToDoPage))
   }
 
-  def usingService(origin: String) =  Action { implicit request =>
-    Ok(html.feedbackSurvey.usingService(formMappings.usingServiceForm, origin))
-  }
+  def usingService(origin: String): Action[AnyContent] = Action { implicit request => renderPageLoad(origin, usingServicePage) }
 
-  def usingServiceContinue(origin: String) = Action (parse.form(formMappings.usingServiceForm)) { implicit request =>
+  def usingServiceContinue(origin: String): Action[UsingService] = Action(parse.form(formMappings.usingServiceForm)) { implicit request =>
     val beforeUsingThisService = request.body.beforeUsingThisService
-    var option0, option1, option2, option3, option4, option5, option6: (String,String) = ("","")
-    if (beforeUsingThisService.lift(0).isDefined) {option0 = beforeUsingThisService.lift(0).get -> "Checked"}
-    if (beforeUsingThisService.lift(1).isDefined) {option1 = beforeUsingThisService.lift(1).get -> "Checked"}
-    if (beforeUsingThisService.lift(2).isDefined) {option2 = beforeUsingThisService.lift(2).get -> "Checked"}
-    if (beforeUsingThisService.lift(3).isDefined) {option3 = beforeUsingThisService.lift(3).get -> "Checked"}
-    if (beforeUsingThisService.lift(4).isDefined) {option4 = beforeUsingThisService.lift(4).get -> "Checked"}
-    if (beforeUsingThisService.lift(5).isDefined) {option5 = beforeUsingThisService.lift(5).get -> "Checked"}
-    if (beforeUsingThisService.lift(6).isDefined) {option6 = beforeUsingThisService.lift(6).get -> "Checked"}
+    var option0, option1, option2, option3, option4, option5, option6: (String, String) = ("", "")
+    //scalastyle:off magic.number
+    if (beforeUsingThisService.headOption.nonEmpty) {
+      option0 = beforeUsingThisService.headOption.get -> "Checked"
+    }
+    if (beforeUsingThisService.lift(1).isDefined) {
+      option1 = beforeUsingThisService.lift(1).get -> "Checked"
+    }
+    if (beforeUsingThisService.lift(2).isDefined) {
+      option2 = beforeUsingThisService.lift(2).get -> "Checked"
+    }
+    if (beforeUsingThisService.lift(3).isDefined) {
+      option3 = beforeUsingThisService.lift(3).get -> "Checked"
+    }
+    if (beforeUsingThisService.lift(4).isDefined) {
+      option4 = beforeUsingThisService.lift(4).get -> "Checked"
+    }
+    if (beforeUsingThisService.lift(5).isDefined) {
+      option5 = beforeUsingThisService.lift(5).get -> "Checked"
+    }
+    if (beforeUsingThisService.lift(6).isDefined) {
+      option6 = beforeUsingThisService.lift(6).get -> "Checked"
+    }
+    //scalastyle:on magic.number
     audit("feedback-survey", Map(
       "origin" -> origin,
       option0, option1, option2, option3, option4, option5, option6
     ).filter((t) => t._1 != ""), eventTypeSuccess)
-    Redirect(routes.FeedbackSurveyController.aboutService(origin))
+
+    redirectToPage(origin, nextPage(origin, usingServicePage))
   }
 
-  def aboutService(origin: String) = Action { implicit request =>
-    Ok(html.feedbackSurvey.aboutService(formMappings.aboutServiceForm, origin))
-  }
+  def aboutService(origin: String): Action[AnyContent] = Action { implicit request => renderPageLoad(origin, aboutServicePage) }
 
-  def aboutServiceContinue(origin: String) =  Action (parse.form(formMappings.aboutServiceForm)) { implicit request =>
+  def aboutServiceContinue(origin: String): Action[AboutService] = Action(parse.form(formMappings.aboutServiceForm)) { implicit request =>
     val serviceReceived = request.body.serviceReceived
     audit("feedback-survey", Map("origin" -> origin,
       "serviceReceived" -> serviceReceived.getOrElse("")), eventTypeSuccess)
-    Redirect(routes.FeedbackSurveyController.recommendService(origin))
+    ///feedback-survey/recommendService/PODS
+    redirectToPage(origin, nextPage(origin, aboutServicePage))
+
   }
 
-  def recommendService(origin: String) = Action { implicit request =>
-    Ok(html.feedbackSurvey.recommendService(formMappings.recommendServiceForm, origin))
-  }
+  def recommendService(origin: String): Action[AnyContent] = Action { implicit request => renderPageLoad(origin, recommendServicePage) }
 
-  def recommendServiceContinue(origin: String) =  Action (parse.form(formMappings.recommendServiceForm)) { implicit request =>
+  def recommendServiceContinue(origin: String): Action[RecommendService] = Action(parse.form(formMappings.recommendServiceForm)) { implicit request =>
     val reasonForRating = request.body.reasonForRating
     val recommendRating = request.body.recommendRating
     audit("feedback-survey", Map(
       "origin" -> origin,
       "reasonForRating" -> reasonForRating.getOrElse(""),
       "recommendRating" -> recommendRating.getOrElse("")), eventTypeSuccess)
-
-    originService.customFeedbackUrl(Origin(origin)) match {
-      case Some(x) => Redirect(x)
-      case None => Redirect(routes.FeedbackSurveyController.thankYou(Origin(origin)))
-    }
+    redirectToPage(origin, nextPage(origin, recommendServicePage))
   }
 
-  def thankYou(origin : Origin): Action[AnyContent] = Action {
+  def thankYou(origin: Origin): Action[AnyContent] = Action {
     implicit request =>
-      if(originService.isValid(Origin(origin.origin))) {
+      if (originService.isValid(Origin(origin.origin))) {
         Ok(html.feedbackSurvey.thankYou(FrontendAppConfig.urLinkUrl, origin.origin))
       } else {
         Ok(html.error_template("global_errors.title", "global_errors.heading", "global_errors.message"))
       }
   }
-
 }
